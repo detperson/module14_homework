c3a81e94ea10368eefbc1a9c1294b6d4b4d6b397
Ilya Tkachenko <106911235+detperson@users.noreply.github.com>
2023-01-03 16:53:00 +0300

    Сorrections after mentor checking

--- 14.1 Задание 2.js
+++ 14.1 Задание 2.js
@@ -22,26 +22,29 @@ const jsonString = `
 /* Этап 2. Получение данных */
 const data = JSON.parse(jsonString)
 // console.log('data', data)
-const list = data.list
-// console.log('list', list)
-const person1 = list[0]
-const person2 = list[1]
-// console.log('person1', person1)
 
-
-/* Этап 3. Запись данных в результирующий объект */
-const result = {
-    list: [
-        {
-            name: person1.name,
-            age: Number(person1.age),
-            prof: person1.prof
-        },
-        {
-            name: person2.name,
-            age: Number(person2.age),
-            prof: person2.prof
-        },
-    ]
-}
-console.log('result', result)
+// -----Исправления после проверки ментором-----
+// const list = data.list
+// // console.log('list', list)
+// const person1 = list[0]
+// const person2 = list[1]
+// // console.log('person1', person1)
+//
+//
+// /* Этап 3. Запись данных в результирующий объект */
+// const result = {
+//     list: [
+//         {
+//             name: person1.name,
+//             age: Number(person1.age),
+//             prof: person1.prof
+//         },
+//         {
+//             name: person2.name,
+//             age: Number(person2.age),
+//             prof: person2.prof
+//         },
+//     ]
+// }
+// console.log('result', result)
+console.log('result', data)
